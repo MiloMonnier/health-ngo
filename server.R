d9ba2f6732cab6a39b99258542fc53e80eb5be27
MiloMo <milo.monnier@irstea.fr>
2020-09-29 13:54:30 +0000

    Add switch from prop. ircles to choro. map

--- server.R
+++ server.R
@@ -4,30 +4,52 @@ library(sf)
 
 function(input, output, session) {
   
+  dat = reactive({
+    reg
+  })
   # Create the static map, which will be loaded once at startup and then cached
   output$map = renderLeaflet({
-    bbox = as.vector(st_bbox(reg)) # Retrieve the extent of the Senegal
+    bbox = as.vector(st_bbox(dat())) # Retrieve the extent of the Senegal
     leaflet() %>%
-      addTiles(urlTemplate="//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+      addTiles("//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
                attribution='Maps by <a href="http://www.mapbox.com/">Mapbox</a>') %>%
       fitBounds(bbox[1], bbox[2], bbox[3], bbox[4])
   })
   
-  # On the static leaflet map previously created, add the data: regions, contour, circles
+  # On the static leaflet map previously created, add the data (circles or choropleth map)
   # Incremental changes to the map use should be performed in an observer
   observe({
-    leafletProxy("map", data=reg) %>%
-      # Add the 14 senegalese regions
-      addPolygons(color="black", weight=2, opacity=0.5, 
-                  fillColor="blue", fillOpacity=0.05) %>% 
-      # Add the senegalese border with a thicker contour above
-      addPolygons(data=sen, color="black", weight=4, opacity=0.5, 
-                  fillColor="blue", fillOpacity=0.05) %>% 
-      # On each region centroid (lng-lat), add circles proportionnal to the number of NGOs
-      addCircleMarkers(~lng, ~lat, radius=~nb_ong/10,
-                       color="red", weight=1, opacity=0.5,
-                       fillColor="red", fillOpacity=0.3)
+    
+    # Take back the static leaflet map and, every time input$ or dat() changes, clear polygons, markers, and legend 
+    map = leafletProxy("map", data=dat()) %>% 
+      clearShapes() %>% 
+      clearMarkers() %>%
+      clearControls()    # Clears legend
+    # If circles chosen, add circles proportionnal to the number of NGOs on each region centroid (lng-lat)
+    if (input$maptype=="circles") {
+      map %>% 
+        addPolygons(color="black", weight=2, opacity=0.5, fillColor="blue", fillOpacity=0.05) %>%
+        addCircleMarkers(~lng, ~lat, radius=~nb_ong/10, color="red", weight=1, opacity=0.5, fillColor="red", fillOpacity=0.3)
+      
+      # Else, display a choropleth map
+    } else if (input$maptype=="density") {
+      # Set the color discretisation with Jenks algorithm
+      var = dat()$dens_km
+      nclass = 5
+      intervals = classIntervals(var, nclass, style="jenks")
+      classes = cut(var, intervals$brks, include.lowest=TRUE)
+      colors = brewer.pal(nclass, "YlOrRd")
+      col = colors[classes]
+      # Add the Polygons and the Legend
+      map %>%
+        addPolygons(color="black", weight=2, opacity=0.3, fillColor=col, fillOpacity=0.5) %>%
+        addLegend(position="bottomleft", colors=rev(colors), labels=rev(levels(classes)))
+    }
+    
+    # Endly, add the senegalese external border with a thicker contour
+    map %>%
+      addPolygons(data=sen, color="black", weight=4, opacity=0.5, fillOpacity=0) 
   })
-
-  # TODO Add a legend for proportionnal circles radius
+  
+  # TODO Add a click event on senegalese regions
 }
--- ui.R
+++ ui.R
@@ -1,10 +1,36 @@
 library(leaflet)
 
+
+# We use bootstrapPage rather than fluidPagein order to give full extent to the map
+# with custom CSS and finer control over the body (100%-100%)
 bootstrapPage(
+# navBarPage(
   
   tags$style(type="text/css", "html, body {width:100%;height:100%}"),
   
   # If not using custom CSS, set height of leafletOutput to a number instead of percent
-  leafletOutput("map", width = "100%", height = "100%")
+  leafletOutput("map", width="100%", height="100%"),
+  
+  # Add a control panel on the left to set the map representation
+  absolutePanel(id="controls", class="panel panel-default",# Set roads colors
+                fixed=TRUE, draggable=TRUE,
+                top=100, left=20, right="auto", bottom="auto",
+                width="auto", height="auto",
+                
+                # Let the user set the representation mode of the data: proportionnal 
+                # circles or choropleth map. 
+                radioButtons("maptype", "Map representation:",
+                             c("Proportionnal circles"="circles",
+                               "Density"="density")
+                ),
+                # If density choropleth map is chosen, let the choice between /hab or /km2
+                conditionalPanel("input.maptype == 'density'",
+                                 radioButtons("chorotype", NULL,
+                                              c("/km2"="km",
+                                                "/hab"="hab")
+                                 )
+                )
+  )
+  
   
 )
