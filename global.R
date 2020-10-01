832b94d8306b9a99c912d6116c148cacdac3f392
MiloMo <milo.monnier@irstea.fr>
2020-10-01 12:36:35 +0000

    update global.R

--- global.R
+++ global.R
@@ -1,9 +1,11 @@
 library(sf)
+library(classInt)
+library(RColorBrewer)
 
 # Load the data: the health NGOs table, the 14 senegalese regions, and the senegalese 
-ong = read.csv("data/ngos.csv")
-reg = st_read("data/senegal_regions.gpkg", quiet=TRUE)
-sen = st_read("data/senegal_contour.gpkg", quiet=TRUE)
+ngo = read.csv("data/ngos.csv", stringsAsFactors=FALSE)
+reg = st_read("data/senegal_regions.gpkg", stringsAsFactors=FALSE, quiet=TRUE)
+sen = st_read("data/senegal_contour.gpkg", stringsAsFactors=FALSE, quiet=TRUE)
 
 # Compute the centroid of each region and retrieve coordinates.
 # Conventionnaly, centroid should be computed in planar coordinates, in the EPSG:32628
@@ -11,4 +13,18 @@ sen = st_read("data/senegal_contour.gpkg", quiet=TRUE)
 centros = suppressWarnings(st_coordinates(st_centroid(reg)))
 reg$lng = centros[,1]
 reg$lat = centros[,2]
+
+# Compute the density of NGOS per km2, and per 100000/hab
+reg$area_km2 = st_area(st_transform(reg, 32628))/10^6
+reg$area_km2 = as(reg$area_km2, "numeric") 
+reg$dens_km = reg$nb_ong / reg$area_km2
+
+
+# For a continuous gradient
+# pal = colorNumeric("YlGnBu", dat()$dens_km)
+# reg$color = pal(reg$dens_km)
+# Source: https://rstudio.github.io/leaflet/legends.html
+
+
+
 
