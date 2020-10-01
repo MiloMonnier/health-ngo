aa4209672d809146bf2fb97020567d3910aadee5
MiloMo <milo.monnier@irstea.fr>
2020-10-01 12:37:26 +0000

    switch to french

--- server.R
+++ server.R
@@ -2,54 +2,65 @@ library(shiny)
 library(leaflet)
 library(sf)
 
+test = reg
+
 function(input, output, session) {
   
   dat = reactive({
     reg
   })
+  
   # Create the static map, which will be loaded once at startup and then cached
   output$map = renderLeaflet({
-    bbox = as.vector(st_bbox(dat())) # Retrieve the extent of the Senegal
-    leaflet() %>%
-      addTiles("//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+    bbox = as.vector(st_bbox(reg)) # Retrieve the extent of the Senegal
+    leaflet(reg) %>%
+      addTiles(urlTemplate="//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
                attribution='Maps by <a href="http://www.mapbox.com/">Mapbox</a>') %>%
       fitBounds(bbox[1], bbox[2], bbox[3], bbox[4])
   })
   
-  # On the static leaflet map previously created, add the data (circles or choropleth map)
+  
+  # On the static leaflet map previously created, map the data
   # Incremental changes to the map use should be performed in an observer
   observe({
-    
-    # Take back the static leaflet map and, every time input$ or dat() changes, clear polygons, markers, and legend 
-    map = leafletProxy("map", data=dat()) %>% 
-      clearShapes() %>% 
+
+    # Take static map and, every time input$ or dat() changes, clear polygons, markers
+    # and legend. Add also senegalese external border with a thicker contour
+    map = leafletProxy("map", data=dat()) %>%
+      clearShapes() %>%
       clearMarkers() %>%
-      clearControls()    # Clears legend
-    # If circles chosen, add circles proportionnal to the number of NGOs on each region centroid (lng-lat)
+      clearControls() %>%
+      addPolylines(data=sen, color="black", weight=4)
+
+    # If circles chosen, add circles proportionnal to the number
+    #of NGOs on each region centroid (lng-lat)
     if (input$maptype=="circles") {
-      map %>% 
-        addPolygons(color="black", weight=2, opacity=0.5, fillColor="blue", fillOpacity=0.05) %>%
-        addCircleMarkers(~lng, ~lat, radius=~nb_ong/10, color="red", weight=1, opacity=0.5, fillColor="red", fillOpacity=0.3)
-      
-      # Else, display a choropleth map
+      map %>%
+        addPolygons(color="black", weight=2, opacity=0.5,
+                    fillColor="blue", fillOpacity=0.05,
+                    label=~lib) %>%
+        addCircleMarkers(~lng, ~lat, radius=~nb_ong/10,
+                         color="red", weight=1, opacity=0.5,
+                         fillColor="red", fillOpacity=0.3,
+                         label=~lib,
+                         popup=paste("Nombre d'ONG:", dat()$nb_ong))
+
+    # Else, display a choropleth map, discretized with Jenks algorithm, and corresponding legend
     } else if (input$maptype=="density") {
-      # Set the color discretisation with Jenks algorithm
       var = dat()$dens_km
       nclass = 5
       intervals = classIntervals(var, nclass, style="jenks")
       classes = cut(var, intervals$brks, include.lowest=TRUE)
       colors = brewer.pal(nclass, "YlOrRd")
       col = colors[classes]
-      # Add the Polygons and the Legend
       map %>%
-        addPolygons(color="black", weight=2, opacity=0.3, fillColor=col, fillOpacity=0.5) %>%
-        addLegend(position="bottomleft", colors=rev(colors), labels=rev(levels(classes)))
+        addPolygons(color="black", weight=2, opacity=0.3,
+                    fillColor=col, fillOpacity=0.5,
+                    label=~lib) %>%
+        addLegend("bottomleft", title="Nb d'ONG/km²",
+                  colors=rev(colors),  labels=rev(levels(classes)))
     }
-    
-    # Endly, add the senegalese external border with a thicker contour
-    map %>%
-      addPolygons(data=sen, color="black", weight=4, opacity=0.5, fillOpacity=0) 
+  
   })
   
-  # TODO Add a click event on senegalese regions
 }
--- ui.R
+++ ui.R
@@ -4,7 +4,6 @@ library(leaflet)
 # We use bootstrapPage rather than fluidPagein order to give full extent to the map
 # with custom CSS and finer control over the body (100%-100%)
 bootstrapPage(
-# navBarPage(
   
   tags$style(type="text/css", "html, body {width:100%;height:100%}"),
   
@@ -19,17 +18,17 @@ bootstrapPage(
                 
                 # Let the user set the representation mode of the data: proportionnal 
                 # circles or choropleth map. 
-                radioButtons("maptype", "Map representation:",
-                             c("Proportionnal circles"="circles",
-                               "Density"="density")
-                ),
-                # If density choropleth map is chosen, let the choice between /hab or /km2
-                conditionalPanel("input.maptype == 'density'",
-                                 radioButtons("chorotype", NULL,
-                                              c("/km2"="km",
-                                                "/hab"="hab")
-                                 )
+                radioButtons("maptype", "Type de représentation:",
+                             c("Cercles proportionnels"="circles",
+                               "Densité"="density")
                 )
+                # If density choropleth map is chosen, let the choice between /hab or /km2
+                # conditionalPanel("input.maptype == 'density'",
+                #                  radioButtons("chorotype", NULL,
+                #                               c("/km2"="km",
+                #                                 "/hab"="hab")
+                #                  )
+                # )
   )
   
   
